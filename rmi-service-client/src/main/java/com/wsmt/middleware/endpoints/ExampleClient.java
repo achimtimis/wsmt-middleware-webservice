050f19b7b4267442cc568fae937cd6573f5b956a
atimis <achytimis@gmail.com>
2018-06-07 21:11:52 +0300

    added command line runner skeleton

--- rmi-service-client/src/main/java/com/wsmt/middleware/endpoints/ExampleClient.java
+++ rmi-service-client/src/main/java/com/wsmt/middleware/endpoints/ExampleClient.java
@@ -9,13 +9,14 @@ import org.springframework.remoting.rmi.RmiProxyFactoryBean;
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
 import java.util.List;
+import java.util.Scanner;
 
 @Configuration
 public class ExampleClient {
 
 
     @Bean
-    public FileServiceBean fileServiceBean(){
+    public FileServiceBean fileServiceBean() {
         return new FileServiceBean();
     }
 
@@ -24,7 +25,7 @@ public class ExampleClient {
         RmiProxyFactoryBean rpfb = new RmiProxyFactoryBean();
         rpfb.setServiceInterface(IFileService.class);
         String hostAddress = Inet4Address.getLocalHost()
-                                         .getHostAddress();
+                .getHostAddress();
         rpfb.setServiceUrl(String.format("rmi://%s:2099/IFileService", hostAddress));
         return rpfb;
     }
@@ -40,4 +41,31 @@ public class ExampleClient {
         System.out.println(bean.filterFileInfo("file1", "", "", ""));
     }
 
+    public static void runConsole() {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(ExampleClient.class);
+        FileServiceBean bean = context.getBean(FileServiceBean.class);
+        System.out.println("Middleware console");
+        System.out.println("Options: 1. Index files 2. Filter files");
+
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        switch (i) {
+            case 1:
+                System.out.println("Insert context root:");
+                String contextRootInput = sc.next();
+                try {
+                    bean.indexFilesUnderPath(contextRootInput);
+                } catch (Exception e) {
+
+                }
+                break;
+            case 2:
+
+                break;
+
+            default:
+                runConsole();
+        }
+    }
 }
